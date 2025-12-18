8a01be0876e58eadd37bb6941da820a33837e38a
vishnuethineni <vishnuethineni@gmail.com>
2025-12-18 15:09:00 +0530

    commit

--- multilinearRegression.py
+++ multilinearRegression.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import matplotlib.pyplot as plt
+import matplotlib as plt
 import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
